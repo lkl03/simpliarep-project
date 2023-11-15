99b4cf7e7625eea76949ddee846dbb417990a24b
Luca <lulago17@gmail.com>
2023-11-15 02:45:51 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openOverlay(overlayId) {
+    document.getElementById(overlayId).style.display = 'block';
+}
+
+// Function to close a specific overlay
+function closeOverlay(overlayId) {
+    document.getElementById(overlayId).style.display = 'none';
+}
